8a2ff49a98ed70e0a5ffe19748e8e7b5f476e90c
Egor Gladilin <work@devbear.ru>
2022-05-23 23:21:55 +0300

    feat: done 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,44 @@
+const fs = require('fs');
+const path = require('path');
 
+const folderPath = path.join(__dirname, '/files');
+const folderCopyPath = path.join(__dirname, '/files-copy');
+
+const copyDir = (folderPath, folderCopyPath) => {
+  fs.rm(folderCopyPath, { recursive: true, force: true }, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    fs.mkdir(folderCopyPath, { recursive: true }, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+    });
+
+    fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        return console.error(err);
+      }
+
+      files.forEach((file) => {
+        const filePath = path.join(folderPath, file.name);
+        const fileCopyPath = path.join(folderCopyPath, file.name);
+
+        if (file.isFile()) {
+          fs.copyFile(filePath, fileCopyPath, (err) => {
+            if (err) {
+              return console.error(err);
+            }
+          });
+        }
+
+        if (file.isDirectory()) {
+          copyDir(filePath, fileCopyPath);
+        }
+      });
+    });
+  });
+};
+
+copyDir(folderPath, folderCopyPath);
